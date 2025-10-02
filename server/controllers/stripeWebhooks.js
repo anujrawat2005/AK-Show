d5cb2e1d839bc581108400dd75e5b854c20db278
anujrawat2005 <anujrawat724@gmail.com>
2025-10-02 22:38:20 +0530

    further updates

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -38,7 +38,6 @@ export const stripeWebhooks = async(request,response)=>{
 
             default:
                 console.log('Unhandled event type:',event.type)
-            
         }
         response.json({received:true})
     } catch (err) {
